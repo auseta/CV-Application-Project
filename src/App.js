50ce1b204d4700ddb09959f4314904add80bcabb
Augusto Seta <alseta97@gmail.com>
2022-12-07 22:51:34 -0300

    Created the main components, added styles and an external font for the application display.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Content from './components/Content';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import './styles/style.css'
 
-function App() {
-  return (
-    <div className='app-wrapper'></div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className='app-wrapper'>
+        <Header/>
+        <Content />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='app--footer'>
+        <p>⚛ Project made it by Augusto Seta ⚛</p>
+      </div>
+    )
+  }
+}
