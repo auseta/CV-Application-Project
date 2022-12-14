3cffd87c9c081861715bfa0ce26faa011bfef1f8
Augusto Seta <alseta97@gmail.com>
2022-12-14 15:34:28 -0300

    a part of the component has been drawn.

--- src/components/contentComponents/Resume.js
+++ src/components/contentComponents/Resume.js
@@ -4,7 +4,17 @@ export default class Resume extends React.Component {
   render() {
     return (
       <div className='resume'>
-       
+        <div className='resume--tile--one'>
+          <div>
+            <h2>Augusto L. Seta</h2>
+            <p>Web Developer</p>
+          </div>
+          <ul>
+            <li><i class="fas fa-envelope"></i>augustoseta97@gmail.com</li>
+            <li><i class="fas fa-phone"></i>+54 2477 537808</li>
+            <li><i class="fas fa-map-marker-alt"></i>Carlos Gardel 1648</li>
+          </ul>
+        </div>
       </div>
     )
   }
