dad7135ea607df62a27df787a02fa7d77c9d90ee
Augusto Seta <alseta97@gmail.com>
2022-12-15 21:46:26 -0300

    The application state was declared

--- src/components/Content.js
+++ src/components/Content.js
@@ -54,60 +54,62 @@ export default class Content extends React.Component {
     };
 
     this.setName = this.setName.bind(this)
+    this.setJobTitle = this.setJobTitle.bind(this)
     this.submitHandler = this.submitHandler.bind(this)
+
   }
 
   setName(e) {
-    this.setState((prev) => ({
-      ...prev,
-      newData: { ...prev, fullName: e.target.value },
-    }));
+    this.setState(prev => ({...prev, newData : { ...prev.newData, fullName : e.target.value}}))
+  }
+
+  setJobTitle(e) {
+    this.setState(prev => ({...prev, newData : { ...prev.newData, jobTitle : e.target.value }}))
   }
 
   submitHandler(e) {
-    e.preventDefault()
-    this.setState((prev) => {
-      return {
-        ...prev,
-        resumeData: {
-          fullName: prev.newData.fullName,
-          jobTitle: "Web Developer",
-          email: "johnschmoe97@gmail.com",
-          phoneNumber: "+11 1234 567899",
-          address: "Fake Street 123",
-          school: "Massachusetts Institute of Technology",
-          studyTitle: "Systems engineering",
-          startOfStudies: new Date("January 13, 2010").toLocaleDateString(
-            "en-us",
-            { year: "numeric", month: "short", day: "numeric" }
-          ),
-          endOfStudies: new Date("December 18, 2015").toLocaleDateString(
-            "en-us",
-            { year: "numeric", month: "short", day: "numeric" }
-          ),
-          companyName: "Awesome International Systems",
-          jobPosition: "Web Developer",
-          startOfJob: new Date("February 23, 2017").toLocaleDateString(
-            "en-us",
-            { year: "numeric", month: "short", day: "numeric" }
-          ),
-          endOfJob: new Date("November 10, 2022").toLocaleDateString("en-us", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-          }),
-        },
-      };
-    });
+    e.preventDefault();
+    this.setState(prev => ({
+      ...prev,
+      resumeData : {
+        fullName: prev.newData.fullName || 'Joe Schmoe' ,
+        jobTitle: prev.newData.jobTitle || 'Web Developer' ,
+        email: "johnschmoe97@gmail.com",
+        phoneNumber: "+11 1234 567899",
+        address: "Fake Street 123",
+        school: "Massachusetts Institute of Technology",
+        studyTitle: "Systems engineering",
+        startOfStudies: new Date("January 13, 2010").toLocaleDateString(
+          "en-us",
+          { year: "numeric", month: "short", day: "numeric" }
+        ),
+        endOfStudies: new Date("December 18, 2015").toLocaleDateString(
+          "en-us",
+          { year: "numeric", month: "short", day: "numeric" }
+        ),
+        companyName: "Awesome International Systems",
+        jobPosition: "Web Developer",
+        startOfJob: new Date("February 23, 2017").toLocaleDateString("en-us", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        }),
+        endOfJob: new Date("November 10, 2022").toLocaleDateString("en-us", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        }),
+      }
+    }))
   }
 
   render() {
+
     const eventHandlers = {
-      setName: this.setName,
+      setName : this.setName,
+      setJobTitle : this.setJobTitle,
       submitHandler : this.submitHandler
     };
-
-    console.log(this.state.newData);
 
     return (
       <div className="app-wrapper">
