f36f4302b0dda8b4cffc8790166bd1cfcbdc3b9a
Augusto Seta <alseta97@gmail.com>
2022-12-17 15:48:31 -0300

    imported Header and Footer components to App.js component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import './styles/style.css'
 import Content from './components/Content';
+import { Header } from './components/Header';
+import Footer from './components/Footer';
 
 
 class App extends Component {
   render() {
     return (
       <div className='container'>
+        <Header />
         <Content />
+        <Footer />
       </div>
     )
   }
